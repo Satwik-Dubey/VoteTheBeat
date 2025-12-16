a4ab612b35e7ccb10a4037ddee7a4175ceb19abf
Satwik-Dubey <dubeysatwik508@gmail.com>
2025-12-16 23:00:44 +0530

    Updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import sessionRoutes from "./routes/session.routes";
+import voteRoutes from "./routes/vote.routes";
+import sessionSongsRoutes from "./routes/sessionSongs.routes";
+import songRoutes from "./routes/song.routes";
+
+
 
 const app = express();
 app.use(express.json());
@@ -8,6 +13,14 @@ app.use(express.json());
 // Session APIs
 //Any request starting with /sessions is handled by session.routes.ts
 app.use("/sessions", sessionRoutes);
+// Vote APIs
+app.use("/songs", voteRoutes);
+// Add song api
+app.use("/sessions", sessionSongsRoutes);
+// Delete
+app.use("/songs", songRoutes);
+
+
 
 export default app;
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,45 @@
-import dotenv from "dotenv";
-dotenv.config();
-
+import http from "http";
 import app from "./app";
+import {Server} from "socket.io";
+import { Socket } from "dgram";
 
-const PORT = 4000;
+const PORT=4000;
 
-app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+// Create HTTP server using Express App
+const server=http.createServer(app);
+
+// Create Socket.IO server
+const io = new Server(server, {
+  cors: {
+    origin: "*", // frontend later (we’ll do this down)
+  },
 });
+
+// Handlie socket connections
+io.on("connection",(socket)=>{
+  console.log("User connected:",socket.id);
+
+  // user joins a session room
+  /// It puts a connected user into a specific session room So the server can send messages only to users of that session
+  socket.on("join-session",(sessionId:String)=>{  //"join-session" is a built-in Socket.IO event
+    //It adds this socket (user) into a room
+    socket.join(`session:${sessionId}`);
+     console.log(`Socket ${socket.id} joined session:${sessionId}`);
+  });
+
+  // disconnect user
+  socket.on("disconnect",()=>{       // "disconnect" is a built-in Socket.IO event
+    console.log("User disconnected from the session",socket.id);
+
+  });
+
+});
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`Backend + WebSocket running on http://localhost:${PORT}`);
+});
+
+
+export { io };
+
